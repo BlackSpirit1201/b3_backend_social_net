b7bfb5eb9a0f2bed4fe871b98cef2192a55ccccc
BlackSpirit1201 <wilsonpedraza69@gmail.com>
2024-11-12 21:10:22 -0500

    Last commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,26 @@
 import { Router } from "express";
-import { testUser, register, login, profile, listUsers, updateUser} from "../controllers/user.js";
-import { ensureAuth } from "../middlewares/auth.js";
+import { testUser, register, login, profile, listUsers, updateUser, uploadAvatar, avatar } from "../controllers/user.js";
+import { ensureAuth } from '../middlewares/auth.js';
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import pkg from 'cloudinary';
+const { v2: cloudinary } = pkg;
+
+//Configuración de subida de archivos Cloudinary
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'avatars',
+        allowedFormats: ['jpg', 'png', 'jpeg', 'gif'],
+        public_id: (req, file) => 'avatar-'+Date.now()
+    }
+});
+
+//Configurar multer con límites de tamaño de archivos
+const uploads = multer({
+    storage: storage,
+    limits: {fileSize: 1 * 1024 * 1024} // Limitar tamaño a 1mb
+});
 
 const router = Router();
 
@@ -11,5 +31,8 @@ router.post('/login', login);
 router.get('/profile/:id', ensureAuth, profile)
 router.get('/list/:page?', ensureAuth, listUsers)  // "?" es para dejar la variable opcional
 router.put('/update', ensureAuth, updateUser);
+router.post('/upload-avatar', ensureAuth, uploads.single("file0"), uploadAvatar)
+router.get('/avatar/:file', avatar)
+
 //Exportar el Router
 export default router;
